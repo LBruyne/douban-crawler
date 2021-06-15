0d3bc03b66c58004df3692e17e4237a514f664e1
LBruyne <519102931@qq.com>
2021-06-15 14:51:25 +0800

    生成爬虫

--- douban_crawler/douban_crawler/settings.py
+++ douban_crawler/douban_crawler/settings.py
@@ -17,7 +17,7 @@ NEWSPIDER_MODULE = 'douban_crawler.spiders'
 #USER_AGENT = 'douban_crawler (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
@@ -25,12 +25,14 @@ ROBOTSTXT_OBEY = True
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 0.5
+
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
 
 # Disable cookies (enabled by default)
+# 是否使用登陆
 #COOKIES_ENABLED = False
 
 # Disable Telnet Console (enabled by default)
---
+++ douban_crawler/douban_crawler/spiders/douban_spider.py
@@ -0,0 +1,10 @@
+import scrapy
+
+
+class DoubanSpiderSpider(scrapy.Spider):
+    name = 'douban_spider'
+    allowed_domains = ['movie.douban.com']
+    start_urls = ['https://movie.douban.com/']
+
+    def parse(self, response):
+        pass
